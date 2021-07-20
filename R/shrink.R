f46c4f83e7ad5fe80ffbdb374405702c5ce708df
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-07-20 12:38:53 -0400

    added R code for shrink operator

---
+++ R/shrink.R
@@ -0,0 +1,9 @@
+#' Soft shrinkage operator
+#'
+#' @param u a numeric vector of length one
+#' @param alpha a numeric vector of length one
+#' @return a numeric vector of length one, the output of the soft shrinkage operator
+
+shrink <- function(u, alpha){
+  return(sign(u) * max(abs(u) - alpha, 0))
+}
