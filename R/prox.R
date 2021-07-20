cfa762765cae25a9491ce3b4a07e82a9eaf49ab2
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-07-20 13:12:27 -0400

    added prox function

---
+++ R/prox.R
@@ -0,0 +1,10 @@
+#' Perform proximal mapping of rho tau
+#'
+#' @param xi a single number
+#' @param alpha a number
+#' @param tau a number between 0 and 1, the quantile of interest
+#' @return output of the proximal mapping of rho tau
+
+prox <- function(xi, alpha, tau){
+  return(xi - max((tau - 1) / alpha, min(xi, tau / alpha)))
+}
