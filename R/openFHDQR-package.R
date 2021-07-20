39fa19298db9d8a4854a89dec122d5e29466e2b8
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-07-20 12:20:26 -0400

    initial commit

---
+++ R/openFHDQR-package.R
@@ -0,0 +1,14 @@
+## usethis namespace: start
+#' @import RcppEigen
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @useDynLib openFHDQR, .registration = TRUE
+## usethis namespace: end
+NULL
