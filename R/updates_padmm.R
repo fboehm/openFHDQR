ff848f55add4dd8538b54e62cb58f8db41a793d7
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-07-21 11:02:12 -0400

    added step 2.1 to wrapper function

--- R/updates_padmm.R
+++ R/updates_padmm.R
@@ -11,6 +11,7 @@
 #' @param w weights vector
 #' @return updated beta vector
 #' @family proximal ADMM for weighted L1 penalized quantile regression
+#' @export
 
 update_beta_padmm <- function(beta, X, theta, sigma, eta, y, z, lambda, w){
   new_beta <- beta
@@ -26,7 +27,129 @@ update_beta_padmm <- function(beta, X, theta, sigma, eta, y, z, lambda, w){
 #' Update z for the proximal ADMM for weighted L1-penalized quantile regression
 #'
 #' @param y y vector
-#' @param
+#' @param X design matrix
+#' @param beta current state of beta parameter vector (k + 1)
+#' @param theta current state of theta parameter vector (k)
+#' @param sigma sigma constant
+#' @param tau quantile, a number between 0 and 1
+#' @return updated z vector
 #' @family proximal ADMM for weighted L1 penalized quantile regression
+#' @export
+
+update_z_padmm <- function(y, X, beta, theta, sigma, tau){
+  new_z <- numeric()
+  for (i in seq_along(y)){
+    new_z[i] <- prox(xi = y[i] - X[i, ] * beta + theta[i] / sigma,
+                     alpha = length(y) * sigma,
+                     tau = tau)
+  }
+  return(new_z)
+}
+
+#' Update theta for the proximal ADMM for weighted L1-penalized quantile regression
+#'
+#' @param theta current state of theta (k)
+#' @param gamma gamma constant
+#' @param sigma sigma constant
+#' @param X design matrix
+#' @param beta current state of beta (k + 1)
+#' @param z current state of z (k + 1)
+#' @param y y vector
+#' @return updated theta vector
+#' @export
+
+update_theta_padmm <- function(theta, gamma, sigma, X, beta, z, y){
+  return(theta - gamma * sigma * (X %*% beta + z - y))
+}
+
+#' Perform proximal ADMM for weighted L1-penalized quantile regression
+#'
+#' @param beta0 initial value of beta
+#' @param z0 initial value of z
+#' @param theta0 initial value of theta
+#' @param maxiter maximum number of iterations
+#' @param epsilon1 epsilon1 constant for stopping
+#' @param epsilon2 epsilon2 constant for stopping
+#' @return a list with three named components: beta, z, theta
+#' @export
+
+padmm_L1 <- function(beta0, z0, theta0, sigma, maxiter = 10 ^ 5, epsilon1 = 0.001, epsilon2 = 0.001){
+  old_beta <- beta0
+  old_z <- z0
+  old_theta <- theta0
+  beta <- old_beta
+  z <- old_z
+  theta <- old_theta
+  iter <- 0
+  while(iter < max_iter & (!crit1 | !crit2)){
+    ## Step 2.1
+    new_beta <- update_beta_padmm(beta = beta,
+                                  X = X,
+                                  theta = theta,
+                                  sigma = sigma,
+                                  eta = eta,
+                                  y = y,
+                                  z = z,
+                                  lambda = lambda,
+                                  w = w)
+    old_beta <- beta
+    beta <- new_beta
+    ## step 2.2
+    crit1 <- check_criterion1(X = X,
+                              beta = beta,
+                              z = z,
+                              y = y,
+                              epsilon1 = epsilon1,
+                              epsilon2 = epsilon2)
+    crit2 <- check_criterion2(sigma = sigma,
+                              X = X,
+                              z = z,
+                              old_z = old_z,
+                              epsilon1 = epsilon1,
+                              epsilon2 = epsilon2,
+                              theta = theta)
+    iter <- iter + 1
+  }
+}
+
+#' Check stopping criterion 1
+#'
+#' @param X design matrix
+#' @param beta current value of beta vector
+#' @param z current value of z vector
+#' @param y y vector
+#' @param epsilon1 epsilon1 constant value
+#' @param epsilon2 epsilon2 constant value
+#' @return a logical vector of length one. TRUE if criterion 1 is satisfied and FALSE otherwise
+#' @export
+
+check_criterion1 <- function(X, beta, z, y, epsilon1, epsilon2){
+  n <- length(y)
+  return(norm_vec(X %*% beta + z - y) <= sqrt(n) * epsilon1 + epsilon2 * max(norm_vec(X %*% beta), norm_vec(z), norm_vec(y)))
+}
+
+#' Check stopping criterion 2
+#'
+#' @param sigma sigma constant
+#' @param X design matrix
+#' @param z current z vector (k)
+#' @param old_z old z vector (k - 1)
+#' @param epsilon1 epsilon1 constant
+#' @param epsilon2 epsilon2 constant
+#' @param theta current theta vector (k)
+#' @return a logical vector of length one. TRUE if criterion 2 is satisfied and FALSE otherwise
+#' @export
+
+check_criterion2 <- function(sigma, X, z, old_z, epsilon1, epsilon2, theta){
+  p <- ncol(X)
+  return(sigma * norm_vec(t(X) %*% (z - old_z)) <= sqrt(p) * epsilon1 + epsilon2 * norm_vec(t(X) %*% theta))
+}
 
 
+#' Calculate l2 norm of a vector
+#'
+#' @param x a vector
+#' @return a numeric vector of length one, the l2 norm of x
+#' @export
+
+norm_vec <- function(x) sqrt(sum(x^2))
