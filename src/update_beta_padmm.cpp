60368df7a0b4f7747deeb555eb3a33831803b515
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-07-27 14:48:15 -0400

    fixed filename

--- src/update_beta_padmm.cpp
+++ src/update_beta_padmm.cpp
@@ -30,9 +30,9 @@ Eigen::VectorXd update_beta_padmm_Rcpp(Eigen::VectorXd beta,
   Eigen::VectorXd new_beta = beta;
   double denom = sigma * eta;
   for (int i = 0; i < X.rows(); i++) {
-    double arg1 = beta(i) + X(, i) * (theta + sigma * y - sigma * X * beta - sigma * z) / denom;
-    double arg2 =  lambda * w(i) / denom;
-    new_beta(i) = shrink(arg1, arg2);
+    double arg1 = beta[i] + X[, i] * (theta + sigma * y - sigma * X * beta - sigma * z) / denom;
+    double arg2 =  lambda * w[i] / denom;
+    new_beta[i] = shrink(arg1, arg2);
   }
   return new_beta;
 }
