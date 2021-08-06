f0c3fdb3c3d5d3e5b56940ae7f458e66fea520a5
Frederick Boehm <frederick.boehm@gmail.com>
2021-08-05 20:06:20 -0400

    deleted boilerplate comment

--- src/update_beta_padmm.cpp
+++ src/update_beta_padmm.cpp
@@ -5,14 +5,6 @@
 
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
 //
 //' Update beta for the proximal ADMM for weighted L1-penalized quantile regression
 //'
